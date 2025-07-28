575adc145d33d623e300c81d02455c57cda6fc9e
IsMoiL2025 <sheralievismoil6@gmail.com>
2025-07-28 19:19:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -78,4 +78,4 @@ def notify():
         return jsonify({'status': 'error', 'message': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='https://ismoil2025.github.io/Vizitka/notify', port=5001, debug=True)
+   app.run(host='0.0.0.0', port=10000  debug=True)
